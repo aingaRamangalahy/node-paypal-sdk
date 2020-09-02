83b93bc61de7bda2a8da0f0a31c57cfed4da0935
aingaRamangalahy <aingaramangalahy@gmail.com>
2020-09-02 09:43:48 +0300

    add dotenv.dev

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config({ path: path.resolve(process.cwd(), '.env') })
 paypal.configure({
   'mode': 'sandbox', //sandbox or live
   'client_id': process.env.CLIENT_ID,
-  'client_secret':process.env.CLIENT_SECRET
+  'client_secret': process.env.CLIENT_SECRET
 });
 
 const app = express();
